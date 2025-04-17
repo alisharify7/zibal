efba644cc8ceacf9ac2c321cdd2f5844e104459b
ali sharifi <alisharifyofficial@gmail.com>
2025-04-17 21:36:14 +0330

    :fire: update init mongoDB script , add auth for connecting to mongo

--- mongo-init/init-mongo.sh
+++ mongo-init/init-mongo.sh
@@ -1,29 +1,25 @@
-#"""
-#* Zibal Payment Test Task
-#* Provisioning MongoDB
-#* Developer: Ali Sharifi
-#* Email: alisharifyofficial@gmail.com
-#* Website: ali-sharify.ir
-#* GitHub: github.com/alisharify7/zibal
-#* Repository: https://github.com/alisharify7/zibal
-#"""
-
 #!/bin/bash
-set -e
 
-MONGO_USER=${MONGO_INITDB_ROOT_USERNAME:-root}
-MONGO_PASS=${MONGO_INITDB_ROOT_PASSWORD:-example}
+MONGO_USER=${MONGODB_USERNAME:-username}
+MONGO_PASS=${MONGODB_PASSWORD:-admin}
 
-until mongosh --host mongo --username $MONGO_USER --password $MONGO_PASS --eval "print(\"waited for connection\")"
+
+echo $MONGO_PASS
+echo $MONGO_USER
+
+# wait for mongoDB
+sleep 5 # we can remove this if we can add depends_on: service_healthy to mongoDB but just for now
+
+until mongo --host mongo -u "$MONGO_USER" -p "$MONGO_PASS" --eval "print(\"waited for connection\")"
 do
     sleep 1
+    echo "connecting to MongoDB ..."
 done
 
 mongorestore --host mongo \
-             --port 27017 \
-             --username $MONGO_USER \
-             --password $MONGO_PASS \
+             -u "$MONGO_USER" \
+             -p "$MONGO_PASS" \
              --gzip \
-             --archive=/mongo-init/dump/transaction.agz
+             --archive=/mongo-init/transaction.agz
 
 echo "Restore completed successfully!"
